fb23aefe813f1fa36f1d8cadcc02d43141b32fea
Eli_Nor <elinor.kotzott@gmx.de>
2025-04-06 19:13:23 +0200

    added init folder to datamanager directory, added test route to app.py

--- app.py
+++ app.py
@@ -11,5 +11,14 @@ os.makedirs(os.path.join(basedir, 'data'), exist_ok=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db.init_app(app)
 
+@app.route('/')
+def home():
+    return "Welcome to MovieWeb App!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
 #with app.app_context():
 #    db.create_all()
